eaa0f70e39a0f9b692a22c02046bfdbd240566ad
Car36s <tpustrom@gmail.com>
2018-11-12 21:46:15 +0200

    Restart everything b/c me no know webpack

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import Content from './components/Content'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+import './style/reset.css'
+import './style/style.scss'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <Header />
+        <Content />
+        <Footer />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/Content.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import World from './World'
+import Controls from './Controls'
+
+class Content extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			mapDetailLevel: 0,
+			activeTile: '',
+			citiesVisibility: false,
+			citiesLabelsVisibility: false,
+			highlightCitiesOnHover: false,
+		}
+
+		this.setMapDetailLevel = this.setMapDetailLevel.bind(this)
+		this.toggleCitiesVisibility = this.toggleCitiesVisibility.bind(this)
+		this.toggleCitiesLabelsVisibility = this.toggleCitiesLabelsVisibility.bind(this)
+		this.setActiveTile = this.setActiveTile.bind(this)
+		this.onToggleHighLightCities = this.onToggleHighLightCities.bind(this)
+	}
+
+	setMapDetailLevel(level) {
+		this.setState({ mapDetailLevel: level })
+	}
+	toggleCitiesVisibility() {
+		this.setState({ citiesVisibility: !this.state.citiesVisibility })
+	}
+	toggleCitiesLabelsVisibility() {
+		this.setState({ citiesLabelsVisibility: !this.state.citiesLabelsVisibility })
+	}
+	setActiveTile(activeTile) {
+		this.setState({ activeTile })
+	}
+	onToggleHighLightCities() {
+		this.setState({ highlightCitiesOnHover: !this.state.highlightCitiesOnHover })
+	}
+
+	render() {
+		const {
+			activeTile,
+			mapDetailLevel,
+			citiesVisibility,
+			citiesLabelsVisibility,
+			highlightCitiesOnHover,
+		} = this.state
+
+		return (
+            <div className="content">
+                <Controls
+                    mapDetailLevel={mapDetailLevel}
+                    setMapDetailLevel={this.setMapDetailLevel}
+                    toggleCitiesLabelsVisibility={this.toggleCitiesLabelsVisibility}
+                    toggleCitiesVisibility={this.toggleCitiesVisibility}
+                    citiesLabelsVisibility={citiesLabelsVisibility}
+                    citiesVisibility={citiesVisibility}
+                    setActiveTile={this.setActiveTile}
+					highlightCitiesOnHover={highlightCitiesOnHover}
+					onToggleHighLightCities={this.onToggleHighLightCities}
+                    />
+                <World
+                    mapDetailLevel={mapDetailLevel}
+                    citiesVisibility={citiesVisibility}
+                    citiesLabelsVisibility={citiesLabelsVisibility}
+                    activeTile={activeTile}
+					highlightCitiesOnHover={highlightCitiesOnHover}
+                    />
+            </div>
+		)
+	}
+}
+
+export default Content
